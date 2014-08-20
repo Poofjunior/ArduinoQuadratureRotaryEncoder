04aafd8b419493597dd54006c95960b848d90f3c
poofjunior <seeifyoucanmeetme@gmail.com>
2014-08-19 17:03:09 -0700

    rewrote to fix finding initial state

--- RotaryEncoderWithInterrupts/RotaryEncoderWithInterrupts.ino
+++ RotaryEncoderWithInterrupts/RotaryEncoderWithInterrupts.ino
@@ -1,170 +1,98 @@
-/*  
-Joshua Vasquez
-Rotary Encoder With Interrupts
-May 26 to 28, 2012
-*/
+// Stepper Acceleration Test
+// Joshua Vasquez
+// August 19, 2014
+
+#include <math.h>
+
+unsigned int stepperAccel = 200;    // steps per rev per rev
+unsigned int totalSteps = 200;
+
+#define FULL_STEP 0
+#define HALF_STEP 4
+#define QUARTER_STEP 5
+#define EIGHTH_STEP 6
+#define SIXTEENTH_STEP 7
 
 
-// Global Variables. 
-byte Values;
-byte prev_state = 1;  // to deduce rotational direction.
-long index = 0;    
-boolean Clockwise = true;  // currently not used.
-int print_time = 100;  // time between prints to the screen in [ms].
-unsigned long past_time;
+/* PINOUTS */
+const int MS1 = 11;
+const int MS2 = 12;
+const int MS3 = 13;
+const int DIR = 4;
+const int STEP = 5;
+const int outputPins[5] = {MS1, MS2, MS3, DIR, STEP};
 
 
-long prev_index = 0;
-long curr_index = 0;
-int displacement;
+void setUStepFactor( unsigned int ustep)
+{
+  digitalWrite(MS1, (0x01 & ustep));
+  digitalWrite(MS2, ((0x02 & ustep) >> 1));
+  digitalWrite(MS3, ((0x04 & ustep) >> 2));
+}
 
-//To output the (int)displacement to the serial line byte by byte.
-byte MSB;
-byte LSB;
+void step(boolean clockwise)
+{
+  if (clockwise)
+     digitalWrite(DIR, HIGH);
+     
+  digitalWrite(STEP, HIGH);
+  delayMicroseconds(1);
+  digitalWrite(STEP, LOW);
+  digitalWrite(DIR, LOW);
+  delayMicroseconds(1);
+}
+
+unsigned long computeT(unsigned int x, unsigned int accel)
+{
+  return (unsigned long)(1000000 * sqrt( (2 * x) / ( (float)accel)));
+}
 
 
 void setup()
-{ 
-  Serial.begin(57600);
-  Serial.flush();
-  Serial.print("\r I'm ready for action! \r");
-  delay(100);
+{
+  Serial.begin(9600); // baud rate irrelevant for Leonardo.
+  delay(3000);
+  Serial.println("Hello, world!");
   
-// do nothing until we enter State_1 (Pins 2 and 3 both low).      Naturally, there's a better way to enter the FSM in any state by reading the ports, but this will do for now.
-// Then attach default interrupts.
-  while( (Values = PIND & B00001100) != B00000000 );
-  attachInterrupt(0,State_2 , RISING);
-  attachInterrupt(1,State_4 , RISING);
-//
-past_time = millis();
-}
-
-void State_1()
-{ 
-  switch (prev_state)
-  {
-    case 2:
-      index++;
-      // Clockwise = true;
-      prev_state = 1;
-      break;
-    case 4:
-      index--;
-      // Clockwise = false;
-      prev_state = 1;
-      break;
-    default:
-      Serial.println("Error");
-      // should never happen unless we skip indices.
-  }
-  // detach the existing interrupts and reassign new conditions.
-  detachInterrupt(0); detachInterrupt(1); 
-  attachInterrupt(0, State_2, RISING);
-  attachInterrupt(1, State_4, RISING);
-}
-
-void State_2()
-{
-  switch( prev_state)
-  {
-    case 1:
-      index++;
-      // Clockwise = true;
-      prev_state = 2;
-      break;
-    case 3:
-      index--;
-      // Clockwise = false;
-      prev_state = 2;
-      break;
-    default:
-      Serial.println("Error");
-  }
-  detachInterrupt(0); detachInterrupt(1);  
-  attachInterrupt(0, State_1, FALLING);
-  attachInterrupt(1, State_3, RISING);
-
-}
-
-void State_3()
-{
-  switch (prev_state)
-  {
-    case 2:
-      index++;
-      // Clockwise = true;
-      prev_state = 3;
-      break;
-    case 4:
-      index--;
-      // Clockwise = false;
-      prev_state = 3;
-      break;
-    default:
-      Serial.println("Error");
-  }
-  detachInterrupt(0); detachInterrupt(1);  
-  attachInterrupt(0, State_4, FALLING);
-  attachInterrupt(1, State_2, FALLING);
-
-}
-
-void State_4()
-{
-  switch (prev_state)
-  {
-    case 3:
-      index++;
-      // Clockwise = true;
-      prev_state = 4;
-      break;
-    case 1:
-      index--;
-      // Clockwise = false;
-      prev_state = 4;
-      break;
-    default:
-      Serial.println("Error");  
-  }
-  detachInterrupt(0); detachInterrupt(1);  
-  attachInterrupt(0, State_3, RISING);
-  attachInterrupt(1, State_1, FALLING);
+  for (int i = 0; i < 5; ++i)
+    pinMode(outputPins[i], OUTPUT);
+    
+  setUStepFactor(FULL_STEP);
 }
 
 void loop()
 {
-  // Prints the index at the specified interval. 
-  if (millis() >= (past_time + print_time) )
-  // we can't say "==" because the loop might not fall on that precise moment.
+
+  unsigned long delayTime = 0;
+  unsigned long lastDelayTime = 0;
+  unsigned int currStep = 0;
+  delay(3000);
+  
+
+  while (currStep < totalSteps)
   {
-    past_time = millis();
-    // print current displacement
-    displacement = (int)index;
-    
-    // Two output options: binary and ascii
-    
-    // Here's ASCII
-    Serial.println(displacement); 
-    
-    /* // Here's Binary
-    // displacement is two bytes large.  Send both accross the serial.
-    MSB = (displacement >> 8) & 0xff; // grab the MSB
-    LSB = (displacement & 0xff);      // grab te LSB
-    Serial.write(MSB);
-    Serial.write(LSB);
-    */
+    delayTime = computeT(currStep, stepperAccel);
+    ++currStep;
+    //Serial.println(delayTime - lastDelayTime);
+    step(true);
+    delayMicroseconds(delayTime - lastDelayTime);
+    lastDelayTime = delayTime;
+  }  
+    while (currStep > 0)
+  {
+    --currStep;  // avoid overflow by subtracting first
+    delayTime = computeT(currStep, stepperAccel);
+    Serial.println(lastDelayTime - delayTime);
+    step(true);
+    delayMicroseconds(lastDelayTime - delayTime);
+    lastDelayTime = delayTime;
+  }  
 
-    /*
-    Note: this code can only handle displacements within 
-          the range of an int.  If we want to increase the range of 
-          displacements that we want to observe, we need to 
-          make displacement a 4-byte long intead and Serial.write 
-          all four bytes over.  Right now, the code should handle 
-          about 32.768/4 rotations.  Furthermore, we could just have
-          the Arduino serially print the difference between the last
-          two times it send over the data, which would be the difference
-          in displacement, rather than the displacement.
-    */
+/*
+  for (int i = 0; i < 100; ++i)
+  {
+    step(true);
+    delayMicroseconds(50);
   }
-
+*/
 }
